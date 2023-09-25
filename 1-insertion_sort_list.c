f64bdbc28d158f6c90806b8ce4050a1d953b4d53
OBIDIKE-BON <bonifaceobidike@gmail.com>
2023-09-25 23:01:31 +0100

    main.c

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -38,37 +38,6 @@ listint_t *swap(listint_t **list, listint_t *list_p)
 }
 
 /**
- * create_listint - Creates a doubly linked list from an array of integers
- *
- * @array: Array to convert to a doubly linked list
- * @size: Size of the array
- *
- * Return: Pointer to the first element of the created list. NULL on failure
- */
-listint_t *create_listint(const int *array, size_t size)
-{
-	listint_t *list;
-	listint_t *node;
-	int *tmp;
-
-	list = NULL;
-	while (size--)
-	{
-		node = malloc(sizeof(*node));
-		if (!node)
-			return (NULL);
-		tmp = (int *)&node->n;
-		*tmp = array[size];
-		node->next = list;
-		node->prev = NULL;
-		list = node;
-		if (list->next)
-			list->next->prev = list;
-	}
-	return (list);
-}
-
-/**
  * insertion_sort_list - sorts a doubly linked list of integers
  * in ascending order
  * @list: is the list to be sorted
