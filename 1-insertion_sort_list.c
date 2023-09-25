f6af9702ef8783553e6cf11081b482f0cb6f3764
Sadson Mk <sadsonmkumira@yahoo.com>
2023-09-25 06:39:59 -0700

    Adding files

---
+++ 1-insertion_sort_list.c
@@ -0,0 +1,13 @@
+#include "sort.h"
+
+/**
+ * insertion_sort_list - sorts a doubly linked list of integers
+ * in ascending order
+ * @list: is the list to be sorted
+ */
+
+void insertion_sort_list(listint_t **list)
+{
+
+
+}
