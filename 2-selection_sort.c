26df20ea2639002212a9875ed008246632180d3e
OBIDIKE-BON <bonifaceobidike@gmail.com>
2023-09-26 03:30:39 +0100

    update

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -4,30 +4,30 @@
  * selection_sort - sorts an array of integers in ascending order
  * using the Selection sort algorithm
  *
- * @array: giving array to sort
+ * @array: the array to sort
  * @size: giving array size
  * Return: nothing
  */
 void selection_sort(int *array, size_t size)
 {
 	int tmp;
-	size_t i, j, index = 0;
+	size_t i, j, k;
 
 	if (array == NULL || size < 2)
 		return;
 	for (i = 0; i < size - 1; i++)
 	{
-		index = i;
+		k = i;
 		for (j = i + 1; j < size; j++)
 		{
-			if (array[j] < array[index])
-				index = j;
+			if (array[j] < array[k])
+				k = j;
 		}
-		if (index != i)
+		if (k != i)
 		{
 			tmp = array[i];
-			array[i] = array[index];
-			array[index] = tmp;
+			array[i] = array[k];
+			array[k] = tmp;
 			print_array(array, size);
 		}
 	}
